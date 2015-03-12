238ef042635ed893a1e671fb65207c70d7033505
qckstil <richyke88@citromail.hu>
2015-03-12 14:40:27 +0100

    test
    test

---
+++ test/Exam/TestTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tdd\Test\Exam;
+
+/**
+ * Class Test
+ *
+ * @package Tdd\Test
+ */
+class TestTest extends \PHPUnit_Framework_TestCase
+{
+	public function testTest()
+	{
+		$this->assertEquals(true, true);
+	}
+}
