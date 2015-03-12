66c66b41f3674ca0c5e025dc6f2748e5460211a9
qckstil <balazs.topanka@PC236.luxoffice.duodecadits.com>
2015-03-12 16:56:12 +0100

    1. use one line parser method

--- src/Exam/StringParser.php
+++ src/Exam/StringParser.php
@@ -7,5 +7,18 @@ namespace Tdd\Exam;
  */
 class StringParser
 {
+	/**
+	 * Parses one line string.
+	 *
+	 * @param string $stringToParse   The string needed to parse.
+	 *
+	 * @return array   The array containing the parsed string.
+	 */
+	public function parseOneLine($stringToParse)
+	{
+		$parsedString = explode(',', $stringToParse);
+
+		return $parsedString;
+	}
 
 }
