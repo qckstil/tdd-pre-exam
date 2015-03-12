bcddc065ed6a9ea4c1f5dbeeb141c134dcae96a0
qckstil <balazs.topanka@PC236.luxoffice.duodecadits.com>
2015-03-12 16:36:36 +0100

    1. test init

---
+++ test/Exam/StringParserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * The class for testing the string parser class.
+ *
+ * @package Tdd\Test
+ */
+class StringParserTest extends \PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * Checks if the input string is valid for the one line parsing.
+	 *
+	 * @dataProvider oneLineStringDataProvider
+	 */
+	public function testCheckIfOneLineInputStringIsValid()
+	{
+
+	}
+
+	/**
+	 * Data provider for the one line string parser.
+	 *
+	 * @return array
+	 */
+	public function oneLineStringDataProvider()
+	{
+		return array(
+			array('a,b,c', array('a', 'b', 'c')),
+			array('100,982,444,990,1', array('100', '982', '444', '990', '1')),
+			array('Mark,Anthony,marka@lib.de', array('Mark', 'Anthony', 'marka@lib.de'))
+		);
+	}
+}
